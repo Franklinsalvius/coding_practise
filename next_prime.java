9b4095cd3c40d5c0dc2ff40d5f63ac62812a7be7
Franklin Salvius <franklinsalvius@gmail.com>
2020-05-14 03:04:27 +0530

    adding new program to branch

---
+++ next_prime.java
@@ -0,0 +1,56 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class next_prime
+{
+	public static void main(String[] args) {
+	    int n,i,j,m=0;
+	Scanner sc=new Scanner(System.in);
+	n=sc.nextInt();
+	for(i=n;i<=Integer.MAX_VALUE;i++)
+	{
+	    if((prime(i))&&(palin(i)))
+	    {
+	     m=i;
+	     break;
+	    }
+	}
+
+		System.out.println(m);
+	}
+	static boolean prime(int n)
+{
+    int c=1,i;
+    for(i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+        c=0;
+        break;
+        }
+    }
+    if(c==1)
+    return true;
+    else
+    return false;
+}
+
+static boolean palin( int n)
+{
+    int sum=0,i,num=n;
+    while(n>0)
+    {
+        sum=sum*10+n%10;
+        n=n/10;
+    }
+    if(sum==num)
+    return true;
+    else 
+    return false;
+}
+}
