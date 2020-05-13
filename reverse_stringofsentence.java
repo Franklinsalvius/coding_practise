dc11586dfb30d0d05d5cab677965698d782ea3e5
Franklin Salvius <franklinsalvius@gmail.com>
2020-05-14 02:43:15 +0530

    first change is done

--- reverse_stringofsentence.java
+++ reverse_stringofsentence.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-	//	System.out.println("Hello World");
+		System.out.println("Hello World");
 		Scanner sc=new Scanner(System.in);
 		String str;
 		String[] s;
